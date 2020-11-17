aa226b2c341121f0bf334ffecef0697f0003e56f
Foysal Ahammed <foysal.developer@gmail.com>
2020-11-17 18:12:11 +0600

    Initial

--- src/components/Home/ServiceDetail/ServiceDetail.js
+++ src/components/Home/ServiceDetail/ServiceDetail.js
@@ -2,32 +2,34 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBed, faRestroom, faThumbtack } from '@fortawesome/free-solid-svg-icons';
 import './ServiceDetail.css';
+import { url } from '../../../dbURL/dburl';
 
-const ServiceDetail = ({service}) => {
+const ServiceDetail = ({ service }) => {
+    console.log(service)
     return (
         <div className="col-md-4 col-sm-12">
             <div className="my-4">
                 <div className="card shadow">
-                    <img className="card-img-top img-fluid" src= {service.img} alt=""/>
+                    <img className="card-img-top img-fluid" src={`https://dummyimage.com/300x200/000/fff`} alt={service.title} />
                     <div className="card-body">
-                        <h3 style = {{color: "#16322E" , fontWeight: 800}} className="card-title">{service.title}</h3>
-                        <div className = "d-flex">
-                            <FontAwesomeIcon className = "mr-2" icon={faThumbtack}/>
-                            <p className="card-text">Nasirabad H/S, Chittagong</p>
-                        </div>
-                        <div className="d-flex justify-content-between mt-2">
-                        <div className="calendar">
-                            <FontAwesomeIcon className = "mr-2" icon={faBed}/>
-                            <span>3 Bedrooms</span>
-                        </div>
-                        <div className="people">
-                            <FontAwesomeIcon className = "mr-2" icon={faRestroom}/>
-                            <span>2 Bathrooms</span>
-                        </div>
+                        <h3 style={{ color: "#16322E", fontWeight: 800 }} className="card-title">{service.title}</h3>
+                        <div className="d-flex ">
+                            <FontAwesomeIcon className="mr-2" icon={faThumbtack} />
+                            <p className="card-text">{service.location}</p>
                         </div>
                         <div className="d-flex justify-content-between mt-2">
                             <div className="calendar">
-                                <h2 style = {{color: "#275A53" , fontWeight: 800}} classNameName="mt-3   mb-3 p-2">{service.price}</h2>
+                                <FontAwesomeIcon className="mr-2" icon={faBed} />
+                                <span>{service.numberOfBedRooms}</span>
+                            </div>
+                            <div className="people">
+                                <FontAwesomeIcon className="mr-2" icon={faRestroom} />
+                                <span>{service.numberOfBathRooms}</span>
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-between mt-2">
+                            <div className="calendar">
+                                <h2 style={{ color: "#275A53", fontWeight: 800 }} classNameName="my-2">{`$${service.price}`}</h2>
                             </div>
                             <div className="people">
                                 <button type="button" className="getstarted-btn-2">View Details</button>
---
+++ src/dbURL/dburl.js
@@ -0,0 +1,1 @@
+export const url = `https://appartment-hunt-api.herokuapp.com`
