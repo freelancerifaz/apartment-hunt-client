0fc3d9bb79f9d13c68c22e4fd82cf12e2b56bf2d
freelancerifaz <mrifaz369@gmail.com>
2020-11-17 12:40:03 +0600

    master to razwan

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -3,7 +3,7 @@ import Footer from '../../Shared/Footer/Footer';
 import ClientService from '../ClientService/ClientService';
 import ClientServiceDetail from '../ClientServiceDetail/ClientServiceDetail';
 import Header from '../Header/Header';
-import Services from '../Services/Services';
+import Services from '../Services/Services'
 
 const Home = () => {
     return (
