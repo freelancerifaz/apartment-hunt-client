c9cf9c1182430b1de0bad00de76e0f69d5877300
freelancerifaz <mrifaz369@gmail.com>
2020-11-17 21:33:36 +0600

    update firebase file

--- src/components/Login/Login/firebase.config.js
+++ src/components/Login/Login/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBRZ6r3gDCrpvneIEAiYQLY7TIoD0utKqA",
-    authDomain: "apartment-hunt-d1e07.firebaseapp.com",
-    databaseURL: "https://apartment-hunt-d1e07.firebaseio.com",
-    projectId: "apartment-hunt-d1e07",
-    storageBucket: "apartment-hunt-d1e07.appspot.com",
-    messagingSenderId: "408623685942",
-    appId: "1:408623685942:web:696f2d91ba2525bf309397"
+    apiKey: "AIzaSyCDHBWjpKOyHbLCD47pg_MJkV-f502KuCY",
+    authDomain: "creative-agency-2ddc2.firebaseapp.com",
+    databaseURL: "https://creative-agency-2ddc2.firebaseio.com",
+    projectId: "creative-agency-2ddc2",
+    storageBucket: "creative-agency-2ddc2.appspot.com",
+    messagingSenderId: "567365481283",
+    appId: "1:567365481283:web:a89a91678a0810cc5775f6"
 };
 
 export default firebaseConfig;
